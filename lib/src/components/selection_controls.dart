180668e1ebb841fb94acafd055cfa34195f86bba
Emran Imam <emran.imam@gmail.com>
2020-06-10 20:48:27 +0600

    Fixes #12
    Error: The method '_NeuSelectionControls.buildToolbar' has fewer positional arguments than those of overridden method 'TextSelectionControls.buildToolbar'

--- lib/src/components/selection_controls.dart
+++ lib/src/components/selection_controls.dart
@@ -186,6 +186,7 @@ class _NeuSelectionControls extends TextSelectionControls {
     Offset position,
     List<TextSelectionPoint> endpoints,
     TextSelectionDelegate delegate,
+    ClipboardStatusNotifier clipboardStatusNotifier,
   ) {
     assert(debugCheckHasMediaQuery(context));
     assert(debugCheckHasMaterialLocalizations(context));
@@ -219,7 +220,7 @@ class _NeuSelectionControls extends TextSelectionControls {
         ),
         child: _TextSelectionToolbar(
           handleCut: canCut(delegate) ? () => handleCut(delegate) : null,
-          handleCopy: canCopy(delegate) ? () => handleCopy(delegate) : null,
+          handleCopy: canCopy(delegate) ? () => handleCopy(delegate,clipboardStatusNotifier) : null,
           handlePaste: canPaste(delegate) ? () => handlePaste(delegate) : null,
           handleSelectAll:
               canSelectAll(delegate) ? () => handleSelectAll(delegate) : null,
